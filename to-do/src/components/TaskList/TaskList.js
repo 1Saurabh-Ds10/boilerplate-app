12ab5133a16c0d69c9c364d7eb2b092923d683b9
Saurabh <1saurabh.desai@gmail.com>
2018-08-15 23:56:20 +0530

    initial commit

---
+++ to-do/src/components/TaskList/TaskList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class TaskList extends Component {
+  state = {
+    tasks: []
+  }
+
+  render() {
+    return <h1> Tasks </h1>
+  }
+
+}
+
+export default TaskList;
